4575464690a56f00130756424d07f2c7a215109b
Raiyan Hasan <raiyanhasan2006@gmail.com>
2025-04-05 02:26:32 +0530

    updated the backend url

--- app/vite.config.js
+++ app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ai-resume-70ka.onrender.com',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
